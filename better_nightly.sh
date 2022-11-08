990767d718998fe6ec043b8f1add00e453f65ef1
Guoqiang Zhang <guoqiang@fb.com>
2022-11-08 14:14:27 -0800

    save

--- better_nightly.sh
+++ better_nightly.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -xe
-source /etc/profile
-source /data/home/$USER/.bashrc
-source /data/shared/bin/cluster_env.sh
+#source /etc/profile
+#source /data/home/$USER/.bashrc
+#source /data/shared/bin/cluster_env.sh
 export INNER_DIR="${INNER_DIR}"
 export CCACHE_DIR="/scratch/$USER/work/ccache"
 export MAKEFLAGS=-j96
-export USE_LLVM=/usr/lib/llvm-10
+#export USE_LLVM=/usr/lib/llvm-10
 export TORCH_CUDA_ARCH_LIST="8.0"
-export CUDA_HOME=/usr/local/cuda-11.6
+export CUDA_HOME=/usr/local/cuda-11.4
 export PATH=${CUDA_HOME}/bin:${PATH}
 export CUDA_NVCC_EXECUTABLE=${CUDA_HOME}/bin/nvcc
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib:${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
@@ -22,25 +22,26 @@ if [[ $IS_COVERAGE == 1 ]]; then
 fi
 echo "bash: USER=$USER, dtype=$DTYPE, coverage=$IS_COVERAGE, mode=$mode"
 
-scratch_dir="/scratch/$USER"
-top_dir="/scratch/$USER/dashboard"
-work_dir="/scratch/$USER/dashboard/work"
-env_dir="/scratch/$USER/dashboard/env"
-zipped_file="/data/home/$USER/cluster/dashboard.tar.lz4"
-rm -rf $top_dir
+scratch_dir="${HOME}/"
+top_dir="${HOME}/dashboard"
+work_dir="${HOME}/dashboard/work"
+env_dir="${HOME}/dashboard/env"
+zipped_file="~/dashboard.tar.lz4"
+#rm -rf $top_dir
 mkdir -p $top_dir
 mkdir -p $work_dir
 
 # TODO - Add instructions to build dashboard conda env from scratch
 echo "#### bash: unzipping saved env at $work_dir ####"
-cp $zipped_file $scratch_dir
-new_zip_file="/scratch/$USER/dashboard.tar.lz4"
-lz4 -dc < $new_zip_file | tar xf - -C $scratch_dir
+#lz4 -dc < $zipped_file | tar xf - -C $scratch_dir
 
 echo "#### bash: Activating conda environment from $env_dir ####"
+#alias with-proxy='HTTPS_PROXY=http://fwdproxy:8080 HTTP_PROXY=http://fwdproxy:8080 FTP_PROXY=http://fwdproxy:8080 https_proxy=http://fwdproxy:8080 http_proxy=http://fwdproxy:8080 ftp_proxy=http://fwdproxy:8080'
+export HTTPS_PROXY=http://fwdproxy:8080
+export HTTP_PROXY=http://fwdproxy:8080
 conda activate $env_dir
 conda install -y astunparse numpy scipy ninja pyyaml mkl mkl-include setuptools cffi typing_extensions future six requests dataclasses protobuf numba cython || true
-python -m pip install cmake==3.22.5
+conda install -y -c conda-forge cmake==3.22.3
 conda install -y -c pytorch magma-cuda116 || true
 conda install -y -c conda-forge librosa tqdm gh gitpython || true
 conda install -y -c anaconda certifi || true
@@ -48,11 +49,13 @@ conda install -y pandas==1.4.2 pip git-lfs || true
 conda install -y scikit-learn || true
 # Set the LD_LIBRARY PATH to first look into miniconda libs
 # HACK - For some reason I need this
-export LD_LIBRARY_PATH=$env_dir/lib:${LD_LIBRARY_PATH}
+# export LD_LIBRARY_PATH=$env_dir/lib:${LD_LIBRARY_PATH}
 
 echo "#### bash: Cloning Pytorch ####"
 cd $work_dir
-rm -fr torchdynamo # FIXME
+git config --global http.proxy http://fwdproxy:8080
+git config --global https.proxy http://fwdproxy:8080
+git clone https://github.com/pytorch/pytorch
 cd pytorch
 git fetch && git reset --hard origin/master && git submodule sync && git submodule update --init --recursive --jobs 0
 # git apply /data/home/$USER/cluster/dashboard.patch
@@ -73,12 +76,13 @@ export BUILD_TEST=0
 export USE_GOLD_LINKER=1
 export USE_PYTORCH_QNNPACK=0
 export USE_GOLD_LINKER=1
+export MAX_JOBS=80
 export DEBUG=0
 python setup.py clean && python setup.py develop
 
 python -m pip install regex
 cd benchmarks/dynamo
-make -f Makefile_dashboard pull-deps
+make -f Makefile_dashboard clone-deps
 make -f Makefile_dashboard build-deps
 python -m pip uninstall -y transformers
 python -m pip uninstall -y timm
